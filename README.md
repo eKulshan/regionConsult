099d70542a024895a4f8a2112d52978172e3aaeb
Evgeniy Kulshan <eikulshan@gmail.com>
2021-01-17 15:28:41 +0300

    fix locales

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-[![CI](https://github.com/eKulshan/regionConsult/workflows/CI/badge.svg)](https://github.com/eKulshan/backend-project-lvl4/actions)
+[![CI](https://github.com/eKulshan/regionConsult/workflows/CI/badge.svg)](https://github.com/eKulshan/regionConsult/actions)
 
 # regionConsult
 Test task for RegionConsult
+
+## Setup
+
+```sh
+$ make setup
+```
+
+## Run
+
+```sh
+$ make start
+# Open localhost:5000
+```
--- server/locales/ru.js
+++ server/locales/ru.js
@@ -2,20 +2,51 @@ export default {
   translation: {
     appName: 'Приложение',
     flash: {
+      session: {
+        create: {
+          success: 'Вы залогинены',
+          error: 'Неправильный емейл или пароль',
+        },
+        delete: {
+          success: 'Вы разлогинены',
+        },
+      },
       users: {
         create: {
           error: 'Не удалось зарегистрировать пользователя',
           success: 'Пользователь успешно зарегистрирован',
         },
+        update: {
+          error: 'Не удалось обновить пользователя',
+          success: 'Пользователь успешно обновлен',
+        },
+        delete: {
+          error: 'Не удалось удалить пользователя',
+          success: 'Пользователь успешно удален',
+        },
       },
+      authenticateError: 'Доступ запрещён! Пожалуйста, авторизируйтесь.',
+      authorizeError: 'Доступ запрещен! У вас нет прав на это действие.',
     },
     layouts: {
       application: {
         users: 'Пользователи',
+        signIn: 'Вход',
         signUp: 'Регистрация',
+        signOut: 'Выход',
+        updateProfile: 'Изменить профиль',
+        deleteProfile: 'Удалить профиль',
       },
     },
     views: {
+      session: {
+        new: {
+          signIn: 'Вход',
+          submit: 'Войти',
+          email: 'Email',
+          password: 'Пароль',
+        },
+      },
       users: {
         id: 'ID',
         email: 'Email',
@@ -26,12 +57,24 @@ export default {
           email: 'Email',
           password: 'Пароль',
         },
+        edit: {
+          edit: 'Редактировать профиль',
+          submit: 'Сохранить',
+          email: 'Email',
+          password: 'Пароль',
+        },
       },
       welcome: {
         index: {
           hello: 'Приветствие!',
           description: 'Тестовое задание для РегионКонсалт',
         },
+      },
+      errors: {
+        minLength: 'Не должно быть короче 3 символов',
+        format: 'Должно соответствовать формату эмейл',
+        unique: 'Эмейл уже используется',
+        default: 'Неизвестная ошибка',
       },
     },
   },
